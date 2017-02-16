920068b60051dc43fdf262d9f6b669ac4295b0b5
fagarbal <fagarbal@gmail.com>
2017-02-17 00:55:57 +0100

    fix

--- src/Game.ts
+++ src/Game.ts
@@ -1,7 +1,7 @@
 namespace ChatGame {
   export class Game extends Phaser.Game {
       constructor(socket: SocketIOClient.Socket) {
-        super(window.innerWidth, window.innerHeight, Phaser.WEBGL);
+        super(window.innerWidth, window.innerHeight, Phaser.AUTO);
         this.state.add("Boot", ChatGame.Boot);
         this.state.add("Main", ChatGame.Main.bind(this, socket));
 
